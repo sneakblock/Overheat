2bd6b789d13364de0a0e02e3ed4bf2ab19df79da
ActuallyTrue <dotaigbe@icloud.com>
2020-08-31 21:37:21 -0400

    Updated .gitignore and took out

--- Overheat/Plugins/TargetSystemComponent/Source/TargetSystem/Private/TargetSystemTargetableInterface.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright 2018-2019 Mickael Daniel. All Rights Reserved.
-
-#include "TargetSystemTargetableInterface.h"
-
-// Add default functionality here for any ITargetSystemTargetableInterface functions that are not pure virtual.
